664a80ce585e7a972010ffdb5d6a23b9d1c55d8d
Philip Swiezak <philip.swiezak@brickbite.de>
2023-12-07 18:03:18 +0100

    clean-up

--- .output/public/_nuxt/Offers.0e97fa4e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as r}from"./entry.b5608eb4.js";const n={};function o(s,t){return r(),c("h1",null,"Offers")}const _=e(n,[["render",o]]);export{_ as default};
--- .output/public/_nuxt/Orders.a22817be.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as r,o as c}from"./entry.b5608eb4.js";const n={};function o(s,t){return c(),r("h1",null,"Orders")}const a=e(n,[["render",o]]);export{a as default};
