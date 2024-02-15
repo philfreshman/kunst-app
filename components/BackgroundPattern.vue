f7560fb118a52f44adcceffb7bd09cc400017153
Philip Swiezak <philipswiezak@gmail.com>
2024-02-15 17:47:48 +0100

    finish solution 2

--- components/BackgroundPattern.vue
+++ components/BackgroundPattern.vue
@@ -1,17 +1,30 @@
+<script setup>
+import { defineProps } from "vue"
+
+const props = defineProps({
+  color: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+<!--#1C3452-->
 <template>
-  <svg class="h-full w-full stroke-gray-900/10 dark:stroke-white/10" fill="none">
-    <defs>
-      <pattern
-        id="pattern-5c1e4f0e-62d5-498b-8ff0-cf77bb448c8e"
-        x="0"
-        y="0"
-        width="10"
-        height="10"
-        patternUnits="userSpaceOnUse"
-      >
-        <path d="M-3 13 15-5M-5 5l18-18M-1 21 17 3" />
-      </pattern>
-    </defs>
-    <rect stroke="none" fill="url(#pattern-5c1e4f0e-62d5-498b-8ff0-cf77bb448c8e)" width="100%" height="100%" />
-  </svg>
+  <div class="h-full w-full bg-white dark:bg-[#1C3452]">
+    <svg class="h-full w-full stroke-gray-900/10 dark:stroke-white/10" fill="none">
+      <defs>
+        <pattern
+          id="pattern-5c1e4f0e-62d5-498b-8ff0-cf77bb448c8e"
+          x="0"
+          y="0"
+          width="10"
+          height="10"
+          patternUnits="userSpaceOnUse"
+        >
+          <path d="M-3 13 15-5M-5 5l18-18M-1 21 17 3" />
+        </pattern>
+      </defs>
+      <rect stroke="none" fill="url(#pattern-5c1e4f0e-62d5-498b-8ff0-cf77bb448c8e)" width="100%" height="100%" />
+    </svg>
+  </div>
 </template>
--- components/OfferPreviewModal.vue
+++ components/OfferPreviewModal.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import jsPDF from "jspdf"
 import html2canvas from "html2canvas"
-import PreviewWindow from "~/components/PreviewWindow.vue"
 
 const props = defineProps({
   isOpen: {
@@ -21,39 +20,73 @@ const emit = defineEmits<{ closeModal: [] }>()
 const saveToPdf = async () => {
   const divC = document.getElementById("pdf-div")
   if (divC === null) return
-  const canvas = await html2canvas(divC, { scale: 4, dpi: 300 })
+  const canvas = await html2canvas(divC, { scale: 2 })
   const imgData = canvas.toDataURL("image/png", 1)
   const pdf = new jsPDF({
     orientation: "p",
     unit: "mm",
     format: "a4",
-    compress: false
+    compress: true
   })
   pdf.addImage(imgData, "PDF", 0, 0, 210, 297, "angebot", "SLOW") // "NONE" | "FAST" | "MEDIUM" | "SLOW";
   pdf.save("solution1.pdf")
 }
+
+const zoomValue = ref<string>("0.31")
+
+onBeforeMount(() => {
+  const height = window.screen.height
+
+  switch (height) {
+    case 1050:
+      zoomValue.value = "0.265"
+      break
+    case 1200:
+      zoomValue.value = "0.31"
+      break
+    case 900:
+      zoomValue.value = "0.225"
+      break
+    default:
+      zoomValue.value = "0.25"
+  }
+})
+
+onMounted(() => {
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      emit("closeModal")
+    }
+  })
+})
 </script>
 
 <template>
-  <UModal v-model="open" fullscreen>
-    <div class="w-full h-full flex flex-row overflow-scroll">
-      <div class="flex-grow" @click="emit('closeModal')">
-        <BackgroundPattern />
-      </div>
-      <div @click="saveToPdf" id="pdf-div" class="bg-yellow-200 w-[210mm] aspect-ratio shadow-2xl relative">
-        <img class="w-full h=auto" src="../assets/Angebot.png" alt="pdf" />
-        <div class="absolute top-0 left-0 w-full h-full text-black">
-          <PreviewWindow :data />
+  <div v-if="open" class="absolute top-0 left-0 h-full w-full">
+    <div class="absolute top-0 left-0 h-full w-full z-10">
+      <BackgroundPattern />
+    </div>
+    <div class="absolute w-full h-full flex align-middle justify-center z-40" :style="{ zoom: zoomValue }">
+      <div class="relative aspect-ratio parent overflow-scroll">
+        <div id="pdf-div" class="bg-blue-600 shadow-2xl pdf relative">
+          <img class="w-full absolute h=auto" src="../assets/Angebot.png" alt="pdf" />
+
+          <div class="absolute top-0 left-0 w-full h-full text-black" @click="saveToPdf">
+            <PreviewWindow class="z-50" :data />
+          </div>
         </div>
       </div>
-      <div class="flex-grow" @click="saveToPdf">
-        <BackgroundPattern />
-      </div>
+      <!--      <div @click="saveToPdf" class="flex-grow hover:cursor-pointer"></div>-->
     </div>
-  </UModal>
+  </div>
 </template>
 
 <style lang="sass" scoped>
+
+.pdf
+  width: 1860pt
+  height: 2631pt
+
 .aspect-ratio
   aspect-ratio: 2480 / 3508
 </style>
