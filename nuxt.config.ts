9d22306747cc73e40b73e7cf75c38c5986c144d0
philfreshman <philipswiezak@gmail.com>
2024-03-07 13:06:29 +0100

    remove nuxt/image, vercel-deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss", "nuxt-mdi", "@nuxtjs/supabase", "@nuxtjs/i18n", "@nuxt/image"],
+  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss", "nuxt-mdi", "@nuxtjs/supabase", "@nuxtjs/i18n"],
   i18n: {
     vueI18n: "./locales/i18n.config.ts", // if you are using custom path, default
     locales: [
