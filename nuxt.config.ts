a14cc5b3dd8443b237b03e715ae127866fdf34ac
philfreshman <philipswiezak@gmail.com>
2024-03-23 10:47:47 +0100

    add app title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 export default defineNuxtConfig({
   modules: ["@nuxt/ui", "@nuxtjs/tailwindcss", "nuxt-mdi", "@nuxtjs/supabase", "@nuxtjs/i18n"],
+  app: {
+    head: {
+      title: "Kunst App"
+    }
+  },
+  colorMode: {
+    preference: "dark"
+  },
+  components: {
+    global: true,
+    dirs: ["~/components", "~/layouts/components"]
+  },
+  css: ["~/assets/fonts/fonts.sass"],
+  devtools: {
+    enabled: false
+  },
   i18n: {
     vueI18n: "./locales/i18n.config.ts", // if you are using custom path, default
     locales: [
@@ -23,22 +39,5 @@ export default defineNuxtConfig({
       useCookie: true,
       cookieKey: "i18n_redirected"
     }
-  },
-  devtools: {
-    enabled: false
-  },
-  css: ["~/assets/fonts/fonts.sass"],
-  runtimeConfig: {
-    useAuth: false
-  },
-  imports: {
-    autoImport: true
-  },
-  components: {
-    global: true,
-    dirs: ["~/components", "~/layouts/components"]
-  },
-  colorMode: {
-    preference: "dark"
   }
 })
